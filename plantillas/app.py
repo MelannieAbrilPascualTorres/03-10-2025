9b50b64e549f5f24a36460b0b5083c0eb3928a01
MelannieAbrilPascualTorres <24308060610062@cetis61.edu.mx>
2025-10-16 08:34:09 -0500

    2

--- plantillas/app.py
+++ plantillas/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
+
+app.config['SECRET_KEY'] = 'una_clave_secreta_muy_larga_dificil_de_adivinar'
 
 app = Flask(__name__)
 @app.route('/')
@@ -24,6 +26,23 @@ def acerca():
 @app.route('/formulario')
 def crear_cuenta():
     return render_template('formulario.html')
+
+@app.route('/registrando')
+def registram():
+    return ""
+@app.route('/registrando', methdod = ("GET", "POST"))
+def registrame():
+    if request.method == "POST":
+        nombre = request.form["nombre"]
+        apellido = request.form["apellido"]
+        dia = request.form["dia"]
+        mes = request.form["mes"]
+        año = request.form["año"]
+        genero = request.form["mujer"]
+        genero = request.form["hombre"]
+        genero = request.form["otro"]
+        cuenta = request.form["cuenta"]
+        contra = request.form["contra"]
 
 @app.route('/sesion')
 def iniciar_sesion():
